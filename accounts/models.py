7ca5462949173029b307f185c8548210131828a8
Shahajadahasib <shahajadahasib@gmail.com>
2022-03-27 03:48:00 +0600

    model create

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATAGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    catagory = models.CharField(max_length=200, null=True, choices=CATAGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out of delivery', 'Out fot delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    # Customer =
+    # product =
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    staus = models.CharField(max_length=200, null=True, choices=STATUS)
--- crm1/settings.py
+++ crm1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
     'accounts',
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'crm1.urls'
